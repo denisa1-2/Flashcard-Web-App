8ca0c12e0f413c29e0c7e34668f2f3d18613d7ba
denisaradu28 <raduioana2812@gmail.com>
2025-11-10 17:11:40 +0200

    Initial Django project

---
+++ myproject/flashcards/models.py
@@ -0,0 +1,12 @@
+#from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Flashcard(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question
