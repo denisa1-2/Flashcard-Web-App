640200a15bc5c2c2011b60fbe0718cc9aefd3192
unknown <denisaraileanu99@gmail.com>
2025-11-14 02:02:10 +0200

    Raileanu: Functionalitati1+2 full

--- myproject/flashcards/urls.py
+++ myproject/flashcards/urls.py
@@ -4,6 +4,7 @@ from .import views
 urlpatterns = [
     path('create/',views.create_set, name='create_set'),
     path('read/',views.read_sets, name='read_sets'),
-    path('edit/', views.edit_set, name='edit_set'),
-    path('delete/',views.delete_set, name='delete_set'),
+    path('', views.home, name='home'),
+    path('predefinde/', views.predefined_list, name='predefined_list'),
+    path('predefined/<str:set_key>/', views.predefined_set, name='predefined_set'),
 ]
--- myproject/flashcards/views.py
+++ myproject/flashcards/views.py
@@ -5,6 +5,77 @@ from django.contrib import messages
 from .models import FlashcardSet, Flashcard
 from django import forms
 
+predefined_sets = {
+    "POO": {
+        "title": "POO Basisc",
+        "cards": [
+            {"question": "Ce este un obiect?", "answer": "O instanta a unei clase."},
+            {"question": "Ce este o clasa?", "answer": "Un sablon pentru crearea obiectelor."},
+            {"question": "Ce este mostenirea", "answer": "Mecanismul prin care o clasa preia proprietati si metode din alta clasa."},
+            {"question": "Ce este polimorfismul?", "answer": "Capacitatea aceleiasi metode de a se comporta diferit in functie de context."},
+            {"question": "Ce este o interfata?", "answer": "Un tip care defineste doar metode fara implementare."},
+            {"question": "Ce este overriding-ul?", "answer": "Redefinirea unei metode mostenite in clasa copil."},
+            {"question": "Ce este overloading-ul?", "answer": "Definirea mai multor metode cu acelasi nume, dar parametri diferiti."}
+        ]
+    },
+    "python": {
+        "title": "Python Basics",
+        "cards": [
+            {"question": "Ce este o lista in Python?", "answer": "O colectie ordonata si modificabila de elemente."},
+            {"question": "Ce este 'None'?", "answer": "O constanta care reprezinta absenta unei valori."},
+            {"question": "Ce este un modul?", "answer": "Un fisier Python ce contine variabile, functii sau clase reutilizabile."},
+            {"question": "Ce este un dictionar?", "answer": "O colectie de perechi cheie-valoare."},
+            {"question": "Ce face functia pop()?", "answer": "Sterge si returneaza ultimul elemnet din lista."},
+            {"question": "Ce tip de date returneaza functia input()?", "answer": "Intotdeauna un string."},
+            {"question": "Ce este list comprehension?", "answer": "O metoda scurta de a crea liste folosind expresii."}
+        ]
+    },
+    "cyber": {
+        "title": "Cybersecurity",
+        "cards": [
+            {"question": "Ce este cybersecurity?", "answer": "Protejarea sistemelor si datelor impotriva atacurilor."},
+            {"question": "Ce este un firewall?", "answer": "Un sistem care filtreaza traficul de retea."},
+            {"question": "Ce este malware?", "answer": "Software malitios creat pentru a provoca daune"},
+            {"question": "Ce este autentificare?", "answer": "Procesul prin care se verifica identitatea unui utilizator."},
+            {"question": "Ce este un atac DDoS?", "answer": "Suprasolicitarea unui server cu trafic pentru a-l bloca."},
+            {"question": "Ce este hashing-ul?", "answer": "Transformarea datelor intr-o valoare fixa, ireversibila."}
+        ]
+    },
+
+    "so": {
+        "title": "Sisteme de operare",
+        "cards": [
+            {"question": "Ce este un sistem de operare?", "answer": "Software care gestioneaza hardware-ul si ruleazaa aplicatiile."},
+            {"question": "Ce este un thread?", "answer": "Cea mai mica unitate de executie dintr-un proces."},
+            {"question": "Ce este memoria virtuala?", "answer": "Tehnica ce permite folosirea discului ca extensie a RAM-ului."},
+            {"question": "Ce este kernel-ul?", "answer": "Componenta centrala a sistemului de operare."},
+            {"question": "Ce este un deadlock?", "answer": "Situatie in care procesele se blocheaza reciproc."},
+            {"question": "Ce este un sistem de fisiere?", "answer": "Structura prin care OS-ul organizeaza si stocheaza fisierele."}
+        ]
+    }
+
+}
+
+def predefined_list(request):
+    return render(request, "seturi/predefined_sets.html", {
+        "sets": predefined_sets
+    })
+
+def predefined_set(request, set_key):
+    if set_key not in predefined_sets:
+        return render(request, "seturi/predefined_sets.html", {
+            "title": "Set not found",
+            "cards": []
+        })
+
+    selected_set = predefined_sets[set_key]
+    return render(request, "seturi/predefined_sets.html",{
+        "title": selected_set["title"],
+        "cards": selected_set["cards"],
+    })
+
+
+
 class FlashcardSetForm(forms.ModelForm):
     class Meta:
         model = FlashcardSet
@@ -24,6 +95,9 @@ class FlashcardSetForm(forms.ModelForm):
         return name
 
 
+def home(request):
+    return render(request, 'seturi/home.html')
+
 def create_set(request):
     if request.method == 'POST':
         form = FlashcardSetForm(request.POST)
@@ -35,7 +109,7 @@ def create_set(request):
 
             for q, a in zip(questions, answers):
                 if q.strip() and a.strip():
-                    FlashcardSet.objects.create(set=new_set, question=q, answer=a)
+                    Flashcard.objects.create(set=new_set, question=q, answer=a)
 
             return redirect('read_sets')
     else:
@@ -83,3 +157,4 @@ def delete_set(request):
         return redirect('read_sets')
     sets = FlashcardSet.objects.all()
     return render(request, 'seturi/delete_set.html', {'sets': sets})
+
